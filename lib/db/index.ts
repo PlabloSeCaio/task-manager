77ec957d0b4ddc3f05f5092c31ca3bf26294aa90
Pablo <pabloesparzajimenez@gmail.com>
2026-05-18 15:59:27 -0500

    fix: use rejectUnauthorized: false for Supabase SSL

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -3,7 +3,7 @@ import postgres from "postgres";
 import * as schema from "./schema";
 
 const client = postgres(process.env.DATABASE_URL!, {
-  ssl: "require",
+  ssl: { rejectUnauthorized: false },
   prepare: false,
   max_lifetime: 60 * 5,
 });
